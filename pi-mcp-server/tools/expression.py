8a7db7675a8363de3af9e7c7c1766d373d3f4c6a
Renganath Chokkalingam <rengnath1234@gmail.com>
2026-03-23 15:30:13 +0530

    Update expression.py tools to use eye expressions

--- pi-mcp-server/tools/expression.py
+++ pi-mcp-server/tools/expression.py
@@ -1,28 +1,53 @@
 from instances import mcp
+from hardware.eye_disp_runner import start
+from anim_eyes.utils.moods_utils import DEFAULT, SAD, EXCITED, ANGRY
+import threading
+
+eyes = start()
+
 
 def log(expression):
     print(f"[EYES] {expression}")
 
+
+reset_timer = None
+
+def reset_after_delay(delay=5):
+    global reset_timer
+
+    if reset_timer:
+        reset_timer.cancel()  # cancel previous reset
+
+    def reset():
+        eyes.set_mood(DEFAULT)
+        log("reset to default")
+
+    reset_timer = threading.Timer(delay, reset)
+    reset_timer.start()
+
+
 @mcp.tool()
 def happy_eyes():
-    """Display happy eyes"""
-    log("happy")
+    """Display happy and cute eyes"""
+    log("happy eyes")
+    eyes.set_mood(EXCITED)
+    reset_after_delay()
     return {"emotion": "happy"}
+
 
 @mcp.tool()
 def anger_eyes():
     """Display angry eyes"""
     log("angry")
+    eyes.set_mood(ANGRY)
+    reset_after_delay()
     return {"emotion": "angry"}
+
 
 @mcp.tool()
 def sad_eyes():
     """Display sad eyes"""
     log("sad")
+    eyes.set_mood(SAD)
+    reset_after_delay()
     return {"emotion": "sad"}
-
-@mcp.tool()
-def cute_eyes():
-    """Display cute eyes"""
-    log("cute")
-    return {"emotion": "cute"}
